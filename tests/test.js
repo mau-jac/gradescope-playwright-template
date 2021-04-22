0377968ae3d03a61b9b294beeb2fbb7f1e3130ad
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2021-04-21 20:35:44 -0400

    changed test url to local python server

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,7 @@ const { firefox } = require('playwright-firefox');
 (async () => {
     const browser = await firefox.launch();
     const page = await browser.newPage();
-    await page.goto('https://whatmyuseragent.com//');
+    await page.goto('http://0.0.0.0:8000/');
 
     fs.appendFile('../results/results.json', JSON.stringify(obj), (err)=>{
         if (err) throw console.error(err);
